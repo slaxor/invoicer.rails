59912b791d701b2a20232ff82b66afcf6a154f46
Sascha Teske <sascha.teske@gmail.com>
2010-10-14 12:26:38 +0200

    invoice tex format

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,26 +1,26 @@
 class InvoicesController < ApplicationController
   def index
-    render :json => @current_user.customer.find(params[:customer_id]/invoices
+    render :json => @current_user.customer.find(params[:customer_id]).invoices
   end
 
   def show
     respond_to do |format|
-      format.json { render :json => @current_user.customer.find(params[:customer_id]/invoices.find(params[:id])}
+      format.json { render :json => @current_user.customer.find(params[:customer_id]).invoices.find(params[:id])}
       format.tex {
-        @invoice = @current_user.customer.find(params[:customer_id]/invoices.find(params[:id])
+        @invoice = @current_user.customer.find(params[:customer_id]).invoices.find(params[:id])
       }
     end
   end
 
   def create
-    render :json => @current_user.customer.find(params[:customer_id]/invoices << Contact.create(params[:customer.find(params[:invoice])
+    render :json => @current_user.customer.find(params[:customer_id]).invoices << Contact.create(params[:customer.find(params[:invoice])
   end
 
   def update
-    render :json => @current_user.customer.find(params[:customer_id]/invoices.update(params[:id], params[:id], params[:invoice])
+    render :json => @current_user.customer.find(params[:customer_id]).invoices.update(params[:id], params[:id], params[:invoice])
   end
 
   def destroy
-    render :json => @current_user.customer.find(params[:customer_id]/invoices.destroy(params[:id])
+    render :json => @current_user.customer.find(params[:customer_id]).invoices.destroy(params[:id])
   end
 end
