6fb3fb84998597d505e2131879c7655ec5a4a18d
Sascha Teske <sascha.teske@gmail.com>
2010-11-12 13:52:57 +0100

    started a backlog in a mindmap (which could collect all kinds of data regarding this app)

---
+++ doc/invoicer.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1289565680010" ID="ID_268660081" MODIFIED="1289565691026" TEXT="invoicer">
+<node CREATED="1289565692762" ID="ID_1727811882" MODIFIED="1289565698003" POSITION="right" TEXT="backlog">
+<node CREATED="1289565700582" ID="ID_839378694" MODIFIED="1289565857817" TEXT="to reduce repetetive input as an invoice issuer i want to inherit setting from default attributes"/>
+<node CREATED="1289565861904" ID="ID_1177824501" MODIFIED="1289566188001" TEXT="in order to ease the issueing of invoice numbers (which have to be unique) as an invoice issuer i want to get an automatically generated suggestion in my formfield"/>
+</node>
+</node>
+</map>
--- spec/models/service_invoice_item_spec.rb
+++ spec/models/service_invoice_item_spec.rb
@@ -1,6 +1,8 @@
+require 'spec_helper'
 describe ServiceInvoiceItem do
   describe 'associations' do
     it { should belong_to :invoice }
+    it { should have_many :pauses }
   end
 end
 
